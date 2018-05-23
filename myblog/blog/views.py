e050fb8d7d56bb10ed9cb13ed59ee2dac8558d2e
SmartGx <guox0992@163.com>
2018-05-23 22:14:20 +0800

    update setting,urls, index.html

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -3,4 +3,5 @@ from .models import Post
 
 # Create your views here.
 def index(request):
-	return render(request, 'blog/index.html')
+	post_list = Post.objects.all().order_by('-created_time')
+	return render(request, 'blog/index.html', context={'post_list': post_list})
