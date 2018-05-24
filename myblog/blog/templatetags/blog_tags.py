f2239b2ebe18ff4354aa1b3417375fbc752623c7
SmartGx <guox0992@163.com>
2018-05-24 13:29:08 +0800

    使用自定义模板标签修改页面侧边栏

---
+++ myblog/blog/templatetags/blog_tags.py
@@ -0,0 +1,21 @@
+from ..models import Post, Category
+from django import template
+
+register = template.Library()
+
+# 为了能够通过 {% get_recent_posts %} 的语法在模板中调用这个函数，必须按照 Django 的规定注册这个函数为模板标签
+@register.simple_tag
+def get_recent_posts(num=5):
+    return Post.objects.all().order_by('-created_time')[:num]
+
+# 定义归档模板标签
+@register.simple_tag
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+
+# 定义分类模板标签
+@register.simple_tag
+def get_categories():
+    # 别忘了在顶部引入 Category 类
+    return Category.objects.all()
